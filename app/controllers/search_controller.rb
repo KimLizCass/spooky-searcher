f020a095e7f3c5e22cd0001a4b1c65b835f3657a
Kim Cass <Kim.liz.cass@gmail.com>
2019-10-20 21:45:39 +0100

    Initial commit

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,11 @@
+$all_episodes = JSON.parse(File.read('episodes.json'))
+
+class SearchController < ApplicationController
+  def query
+    @episodes = $all_episodes.select { |episode|
+      contains_title = episode["title"].downcase.include? params[:query].downcase
+      contains_author = episode["author"].downcase.include? params[:query].downcase
+      contains_title || contains_author
+    }
+  end
+end
