40218dbb385c3da700e75842afdb2cfadead30bf
Kim Cass <Kim.liz.cass@gmail.com>
2019-10-20 21:13:21 +0100

    Improved search view, added hyperlinks to episodes and to author pages.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 $all_episodes = JSON.parse(File.read('episodes.json'))
+
 class SearchController < ApplicationController
   def query
-    puts params[:query]
     @episodes = $all_episodes.select { |episode|
       contains_title = episode["title"].downcase.include? params[:query].downcase
       contains_author = episode["author"].downcase.include? params[:query].downcase
